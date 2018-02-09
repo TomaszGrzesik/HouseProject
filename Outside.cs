0511e9c92a312c5ad2613c4e27abf4b01a1dcd25
TomaszGrzesik <t.grzesik19@gmail.com>
2018-02-10 00:41:00 +0100

    testing

---
+++ Outside.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseProject
+{
+    class Outside : Location
+    {
+        public Outside(string name, bool hot) : base(name)
+        {
+            this.hot = hot;
+        }
+
+        private bool hot;
+
+        public override string Description
+        {
+            get
+            {
+                string NewDesription = base.Description;
+                if (hot)
+                    NewDesription += "Here is very hot !";
+                return NewDesription; 
+            }
+        }
+
+
+    }
+}
